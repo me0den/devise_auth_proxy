df811dad19e3c6e0954eb113261cc374b614524f
QuangTK <tkquang@fossil.com>
2020-09-29 15:27:03 +0700

    refactor code

--- lib/devise_auth_proxy/model.rb
+++ lib/devise_auth_proxy/model.rb
@@ -8,9 +8,6 @@ module Devise
 
       included do
         attr_reader :current_password, :password
-      end
-
-      module ClassMethod
         def self.find_for_auth_proxy_authentication(env)
           manager = DeviseAuthProxy::Manager.new(self, env)
           manager.find_or_create_user
