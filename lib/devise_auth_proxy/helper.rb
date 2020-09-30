78262a4dec5c79ed08252a5646c430611e22ca09
QuangTK <tkquang@fossil.com>
2020-09-30 11:32:07 +0700

    update logic redirect after logout

--- lib/devise_auth_proxy/helper.rb
+++ lib/devise_auth_proxy/helper.rb
@@ -4,14 +4,8 @@ module DeviseAuthProxy
     # Modify session controller after user log out.
     # To redirect user to a custom url.
     def after_sign_out_path_for(resource_or_scope)
-      DeviseAuthProxy.logout_url if proxy_user_authenticated? and DeviseAuthProxy.logout_url
+      return DeviseAuthProxy.logout_url if DeviseAuthProxy.logout_url
       super
-    end
-
-    private
-
-    def proxy_user_authenticated?
-      DeviseAuthProxy.proxy_user_id(request.env).present?
     end
   end
 end
